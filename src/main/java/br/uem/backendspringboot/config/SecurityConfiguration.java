892ab2f86d4ee4fd6ba49312ecb612a14e53da0c
lelo451 <ra98995@uem.br>
2024-09-03 16:31:16 -0300

    Disable CORS

--- src/main/java/br/uem/backendspringboot/config/SecurityConfiguration.java
+++ src/main/java/br/uem/backendspringboot/config/SecurityConfiguration.java
@@ -1,6 +1,7 @@
 package br.uem.backendspringboot.config;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
@@ -51,20 +52,5 @@ public class SecurityConfiguration {
                 .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
-    }
-
-    @Bean
-    CorsConfigurationSource corsConfigurationSource() {
-        CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:5173", "http://localhost:80"));
-        configuration.setAllowedMethods(List.of("POST, GET, DELETE, PUT, OPTIONS"));
-        configuration.setAllowedHeaders(List.of("Authorization, Content-Type, Accept"));
-        configuration.setMaxAge(3600L);
-
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-
-        source.registerCorsConfiguration("/**",configuration);
-
-        return source;
     }
 }
