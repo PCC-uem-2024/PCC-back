f69819ab8780353b6371df232b6f34f0d6c36df6
gsguilioti <gsguilioti@gmail.com>
2024-09-10 00:09:17 -0300

    consulta solicitacao

--- src/main/java/br/uem/backendspringboot/controller/SolicitacaoController.java
+++ src/main/java/br/uem/backendspringboot/controller/SolicitacaoController.java
@@ -8,6 +8,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/solicitacoes")
 public class SolicitacaoController {
@@ -20,6 +22,21 @@ public class SolicitacaoController {
         Solicitacao solicitation =ConverterSolicitacao.convertSolicitacao(solicitacao);
         solicitation = solicitacaoService.save(solicitation, solicitacao.getIdAluno());
         return ResponseEntity.ok( solicitation);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Solicitacao>> getAll() {
+        return ResponseEntity.ok(solicitacaoService.findAll());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Solicitacao> getById(@PathVariable Integer id) {
+        return ResponseEntity.ok(solicitacaoService.findById(id));
+    }
+
+    @GetMapping("/aluno/{id}")
+    public ResponseEntity<Solicitacao> getByAluno_Id(@PathVariable Long id) {
+        return ResponseEntity.ok(solicitacaoService.findByAluno_Id(id));
     }
 
 }
